e18ef0e4a47418781f8e4961483fe300c3f22863
XAPHNE <suvnkr.xaphene@gmail.com>
2024-09-06 01:57:13 +0530

    Created DrawingFile Model and Migration

---
+++ app/Models/DrawingFile.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DrawingFile extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['drawing_detail_id', 'file_path'];
+
+    public function drawingDetail()
+    {
+        return $this->belongsTo(DrawingDetail::class);
+    }
+}
