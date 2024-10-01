1e19a305d5861bbeda7c09c7158cc4c0b1973ab5
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-01 16:34:34 +0530

    Updated Comment Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,16 +11,16 @@ class Comment extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'drawing_detail_id',  // Make sure this matches the column name in the migration
+        'drawing_file_id',  // Make sure this matches the column name in the migration
         'comment_body',
         'commented_at',
         'commented_by',
         'resubmitted_at',
     ];
 
-    public function drawingDetail()
+    public function drawingFile()
     {
-        return $this->belongsTo(DrawingDetail::class, 'drawing_detail_id');
+        return $this->belongsTo(DrawingFile::class, 'drawing_file_id');
     }
 
     public function commenter()
