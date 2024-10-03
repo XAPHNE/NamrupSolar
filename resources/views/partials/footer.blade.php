3fff24c09174c8e1a047cd69b9c6bf82eabd86ad
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-03 11:15:06 +0530

    Updated footer link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} - <a href="#">{{ config('app.name') }}</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }} - <a href="{{ url('/') }}">{{ config('app.name') }}</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
