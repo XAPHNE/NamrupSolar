83f3fd830a2e78ed18503508a79ab019e2bc8e82
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-09 12:04:43 +0530

    Created project-timeline blade view

--- resources/views/project-timeline.blade.php
+++ resources/views/project-timeline.blade.php
@@ -13,16 +13,25 @@
 @endsection
 
 @section('content')
-<div id="gantt_here" style='width:100%; height:100%;'></div>
+    <div id="gantt_here" style='width:100%; height:100%;'></div>
 @endsection
 
 @push('styles')
-<link href="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css" rel="stylesheet">
+    <link href="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.css" rel="stylesheet">
+    <style type="text/css">
+        .content,
+        .container-fluid {
+            height: 100%;
+            padding: 0px;
+            margin: 0px;
+            overflow: hidden;
+        }
+    </style>
 @endpush
 
 @push('scripts')
-<script src="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js"></script>
-<script type="text/javascript">
-    gantt.init("gantt_here");
-</script>
+    <script src="https://cdn.dhtmlx.com/gantt/edge/dhtmlxgantt.js"></script>
+    <script type="text/javascript">
+        gantt.init("gantt_here");
+    </script>
 @endpush
